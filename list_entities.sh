ab36b8fbd1f6f1a51fc6cd8d37d074f1a74fbba0
qwercik <erykandrzejewski2000@gmail.com>
2017-10-05 13:50:01 +0200

    Add listing groups to list_users.sh and change it name to list_entities.sh

---
+++ list_entities.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# List usual (not-system) users and groups
+
+if [ $EUID != 0 ]
+then
+	echo "Run as root"
+	exit 1
+fi
+
+filename=''
+
+if [ "$1" = "groups" ]; then
+	filename='/etc/group'
+elif [ "$1" = "users" ]; then
+	filename='/etc/passwd'
+else
+	echo 'Incorrect usage'
+	echo "Use: $0 users/groups"
+	exit 1
+fi
+
+cat $filename | \
+while read line; do
+	entity_name=`echo $line | cut -d: -f1`
+	entity_id=`echo $line | cut -d: -f3`
+	
+	if [ $entity_id -ge 1000 ]; then
+		echo $entity_name
+	fi
+done
